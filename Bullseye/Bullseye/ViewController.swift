e66e1d8819e1c4ae3a58644a5b85aeccb972124f
MohamedKhaled563 <mohamedkhlidmkm0@gmail.com>
2020-12-13 23:01:05 +0200

    Adding main objects and connecting slider

--- Bullseye/Bullseye/ViewController.swift
+++ Bullseye/Bullseye/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlert(_ sender: Any) {
+        let message = "The value of the slider is: \(currentValue)"
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: message,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title:"cancel",
+                                   style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
+
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        currentValue = lroundf(sender.value)
+    }
+    
+
+    
 }
 
