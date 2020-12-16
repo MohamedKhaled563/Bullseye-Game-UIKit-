3c952a8c5a5f014536362fa7d9a2ca0c26fa7253
MohamedKhaled563 <mohamedkhlidmkm0@gmail.com>
2020-12-16 18:08:24 +0200

    Adding target, score and round logic

--- Bullseye/Bullseye/ViewController.swift
+++ Bullseye/Bullseye/ViewController.swift
@@ -10,14 +10,41 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentValue: Int = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLable: UILabel!
+    @IBOutlet weak var scoreLable: UILabel!
+    @IBOutlet weak var roundLable: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startNewRound()
     }
 
+    func startNewRound(){
+        round += 1
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updataLables()
+    }
+    
+    func updataLables(){
+        targetLable.text = String(targetValue)
+        scoreLable.text = String(score)
+        roundLable.text = String(round)
+    }
+    
+    
     @IBAction func showAlert(_ sender: Any) {
-        let message = "The value of the slider is: \(currentValue)"
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+        
+        let message = "You scored \(points)"
         let alert = UIAlertController(title: "Hello, World",
                                       message: message,
                                       preferredStyle: .alert)
@@ -26,11 +53,12 @@ class ViewController: UIViewController {
                                    handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
 
 
-    @IBAction func sliderMoved(_ sender: UISlider) {
-        currentValue = lroundf(sender.value)
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
     
 
