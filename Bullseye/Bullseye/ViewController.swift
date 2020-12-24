4d88a0a7e7aa8d64226ade9ce53afdb175257dd6
MohamedKhaled563 <mohamedkhlidmkm0@gmail.com>
2020-12-24 21:15:23 +0200

    Make game style and add new screen

--- Bullseye/Bullseye/ViewController.swift
+++ Bullseye/Bullseye/ViewController.swift
@@ -21,7 +21,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        startNewRound()
+        startNewGame()
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal,
+                             for: .normal)
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right:
+        14)
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
     }
 
     func startNewRound(){
@@ -41,24 +54,51 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert(_ sender: Any) {
         let difference = abs(targetValue - currentValue)
-        let points = 100 - difference
+        var points = 100 - difference
+        
+        let title: String
+        if (difference == 0){
+            title = "Perfect!"
+            // Adding 100 bonus points
+            points += 100
+        }
+        else if difference < 5{
+            title = "You almost had it!"
+            // Add 50 bonus points if difference is 1
+            if (difference == 1){
+                points += 50
+            }
+        }
+        else if difference < 10{
+            title = "Pretty good!"
+        }
+        else{
+            title = "Not even close..."
+        }
+        
         score += points
         
         let message = "You scored \(points)"
-        let alert = UIAlertController(title: "Hello, World",
+        let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .alert)
         let action = UIAlertAction(title:"cancel",
-                                   style: .default,
-                                   handler: nil)
+                                   style: .default){_ in
+            self.startNewRound()
+        }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-        startNewRound()
     }
 
 
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startNewGame(){
+        score = 0
+        round = 0
+        startNewRound()
     }
     
 
