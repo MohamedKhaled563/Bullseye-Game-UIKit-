fd8c4cab8eee36472de79dec2878ff6bba4f62e3
MohamedKhaled563 <mohamedkhlidmkm0@gmail.com>
2021-01-10 07:43:20 +0200

    Editing high score screen

--- Bullseye/Bullseye/EditHighScoreTableViewController.swift
+++ Bullseye/Bullseye/EditHighScoreTableViewController.swift
@@ -7,22 +7,46 @@
 
 import UIKit
 
-class EditHighScoreTableViewController: UITableViewController {
+class EditHighScoreTableViewController: UITableViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     
     }
 
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
     //MARK:- Actions
     
     @IBAction func done(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+        print("\(textField.text!)")
     }
     
     @IBAction func cancel(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    //MARK:- UITextFieldDelegate
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+       
+        return true
     }
 }
 
